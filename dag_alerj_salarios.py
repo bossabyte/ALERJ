f780494164d8c3215114d5d69470321eecf875d5
Juan <coutinholima.j@gmail.com>
2024-01-07 14:03:11 -0300

    add task to limit file download and transformation

--- dag_alerj_salarios.py
+++ dag_alerj_salarios.py
@@ -19,10 +19,18 @@ from airflow.models import Variable
 def alerj_salarios():
 
 
+    gcs_raw_before = GCSListObjectsOperator(
+        task_id='get_raw_files_before',
+        bucket='bossabyte',
+        prefix="raw/",
+        gcp_conn_id="gcp_conn"
+    )
+
     @task(task_id='download_files')
-    def download_files() -> list[tuple[str,str,str]]:
+    def download_files(raw_list) -> list[tuple[str,str,str]]:
         from ALERJ.alerj_modules.alerj_download_file import alerj_download_file
         from itertools import product
+        import re
 
         years = list(range(2016, datetime.now().year))
         months = list(range(1, 13))
@@ -31,6 +39,23 @@ def alerj_salarios():
         months = [1,2]
 
         year_month_list = list(product(years, months))
+
+        files_on_bucket = []
+        for file in raw_list:
+
+            rgx = re.search(r"(?<=folha_)(.*)(?=\.parquet)", file)
+
+            if rgx:
+                year_month_str = rgx.group(0).split('_')
+                files_on_bucket.append((int(year_month_str[0]), int(year_month_str[1])))
+            else:
+                print(file)
+
+
+
+        year_month_list = list(set(year_month_list) - set(files_on_bucket))
+
+        print(year_month_list)
 
         alerj_files = []
 
@@ -67,7 +92,7 @@ def alerj_salarios():
             rmtree(Path(file["src"]).parent)
 
 
-    raw_files = download_files()
+    raw_files = download_files(gcs_raw_before.output)
     parquet_files = pdf_to_parquet.expand(file=raw_files)
 
     upload_to_gcs = LocalFilesystemToGCSOperator.partial(
@@ -90,18 +115,16 @@ def alerj_salarios():
         bucket='bossabyte',
         prefix="trusted/",
         gcp_conn_id="gcp_conn"
+        # match_glob="trusted/**/*.parquet/*"
     )
 
     @task(task_id='files_to_transform')
     def files_to_transform(raw_list, trusted_list):
-        from pathlib import Path
-        print(raw_list)
 
-        raw_file_names = [f"{Path(file).parent.name}/{Path(file).name}" for file in raw_list]
-        trusted_file_names = [f"{Path(file).parents[1].name}/{Path(file).parent.name}" for file in trusted_list]
+        raw_file_names = [file.replace('raw/', '') for file in raw_list]
+        trusted_file_names = [f"{file.split('/')[1]}/{file.split('/')[2]}" for file in trusted_list if len(file.split('/')) >= 2]
 
         trusted_file_names = list(dict.fromkeys(trusted_file_names))
-        print(trusted_file_names)
 
         transform_files = list(set(raw_file_names) - set(trusted_file_names))
 
@@ -123,7 +146,7 @@ def alerj_salarios():
 
     fim = EmptyOperator(task_id='end')
 
-    (raw_files >> parquet_files >> upload_to_gcs >> clean >> gcs_raw_files >> 
+    ( gcs_raw_before >> raw_files >> parquet_files >> upload_to_gcs >> clean >> gcs_raw_files >> 
      gcs_trusted_files >> transform_list >> run_databricks >> fim)
 
     
