fc28da24af42b7eda21361789e0140d27b0d18c0
bossabyte <bossabyte>
2024-01-14 23:47:24 +0000

    Fix the bug related to displayed column numbers and obtain distinct column names for each file.

--- databricks/inspect_alerj_schema.py
+++ databricks/inspect_alerj_schema.py
@@ -1,13 +1,13 @@
 # Databricks notebook source
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 from pathlib import Path
-# dbutils.fs.help("ls")
+import json
+
 dirs = dbutils.fs.ls("gs://bossa-bucket-coutj/trusted", )
 
 files = []
 for folder in dirs:
     files.extend([file_name.path for file_name in dbutils.fs.ls(folder.path)])
-
-print(files)
 
 # COMMAND ----------
 
@@ -16,56 +16,46 @@ dict_schema = {}
 for file in files:
     file_name = Path(file).name
     print("Reading schema:", file_name)
-    schema = sqlContext.read.parquet(file).schema.fields
-    field_names = [field.name for field in schema]
-    dict_schema[file_name] = field_names
-
-
-# fields = sqlContext.read.parquet("gs://bossa-bucket-coutj/trusted/2016/folha_2016_1.parquet").schema.fields
-
-# dict_schema = {}
-# field_names = [field.name for field in fields]
-# # for field in fields:
-# dict_schema["2016"] = field_names
-#     # print(field.name)
-
-print(dict_schema)
-
+    dict_schema[file_name] = spark.read.parquet(file).columns
 
 
 # COMMAND ----------
 
-from pyspark.sql.types import StructType,StructField, StringType, IntegerType
-max_len = 0
+# Create dataframe
 
-for k,v in dict_schema.items():
+# get max column list len
+max_len = 0
+for v in dict_schema.values():
     actual_len = len(v)
     if actual_len > max_len:
         max_len = actual_len
 
-print(max_len)
-
+# Set list to a fix len
 dict_schema_size_fix = {}
 for k,v in dict_schema.items():
-    dict_schema_size_fix[k] = [v[idx] if idx < (len(v)) else "" for idx in range(max_len + 1) ]
+    dict_schema_size_fix[k] = [ v[idx] if idx < len(v) else "" for idx in range(max_len) ]
 
-# print(dict_schema_size_fix)
-
-
-# data = [ tuple(value_list) for value_list in dict_schema.values()]
 data2 = []
 for idx in range(max_len):
     new_list = []
-    for l in dict_schema.values():    
+    for l in dict_schema_size_fix.values():    
         new_list.append(l[idx])
     data2.append(new_list)
 
-# print(data2)
 schema = StructType([ StructField(col_name, StringType(), True) for col_name in dict_schema_size_fix.keys()])
-
-# print(data)
-# print(schema)
 
 df = spark.createDataFrame(data=data2, schema=schema)
 
 display(df)
+
+# COMMAND ----------
+
+#Get list of distinct column names for each file
+
+distinct_values = {}
+
+for idx, list_values in enumerate(data2):
+    distinct_values[f"row_{idx}"] = list(set(list_values))
+
+print(json.dumps(distinct_values))
+
